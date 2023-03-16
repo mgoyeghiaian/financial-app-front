5873cf498a3ba57cd53a5616b47bf6cc397a01f3
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-03-16 10:12:16 +0200

    Added Input And Get And Delete in Income Is Working Fine

--- src/pages/main/components/Income/Report.jsx
+++ src/pages/main/components/Income/Report.jsx
@@ -1,301 +1,174 @@
-import React, { useState, useEffect } from "react";
-import Popup from "./Popup/Popup";
-import Editpopup from "./Popup/Editpopup";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import './income.css'
 const Report = () => {
-  const [title, setTitle] = useState([]);
+  const [title, setTitle] = useState('');
   const [amount, setAmount] = useState([]);
   const [startdate, setstartDate] = useState([]);
   const [enddate, setendDate] = useState([]);
-  const [category, setCategory] = useState([]);
-  const [deleteFixed, setdeleteFixed] = useState([]);
-  const [deleteRecurring, setdeleteRecurring] = useState([]);
-  const [buttonPopup, setButtonPopup] = useState(false);
-  const [editpopup, setEditpopup] = useState(false);
+  const [category, setCategory] = useState('');
+  const [isdeleted, setIsDeleted] = useState(0)
+  const [admin_id, setAdmin_id] = useState(1)
+  const [type, setType] = useState('income')
 
-  const [editPopupId, setEditPopupId] = useState(null);
 
-  let type = "income";
-  // let fixed = "fixed";
-  // let recurring = "recurring";
+  const [RecurringData, setRecurringData] = useState([]);
+  const [fixedData, setfixedData] = useState([]);
+  // const [showConfirmation, setShowConfirmation] = useState(false);
+
+
+  //to Post The data to backend
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    //to send data to backend
-
     if (category === "fixed") {
-      let body = JSON.stringify({
-        title,
-        type,
-        amount,
-        category,
-        enddate,
-      });
+      await axios.post('http://127.0.0.1:8000/api/fixed', {
+        title, amount, enddate, admin_id, isdeleted, category, type
+      })
+      setIsDeleted(false);
+      setAdmin_id(1);
+      setType('income')
+      getfixed();
 
-      let res = fetch("http://127.0.0.1:8000/api/fixed", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: body,
-      });
     }
 
     if (category === "recurring") {
-      let body = JSON.stringify({
-        title,
-        type,
-        amount,
-        category,
-        enddate,
-        startdate,
-      });
-      console.log("body ", body);
-      let res = fetch("http://127.0.0.1:8000/api/recurring", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: body,
-      });
+      await axios.post('http://127.0.0.1:8000/api/recurring', {
+        title, amount, startdate, enddate, admin_id, isdeleted, category, type
+      })
+      setIsDeleted(false);
+      setAdmin_id(1);
+      setType('income')
+      getRecurring();
+
     }
   };
 
-  //to fetch data for fixed
-  const [getfixed, setgetfixed] = useState([]);
 
-  const fetchDataFixed = async () => {
-    try {
-      const response = await fetch("http://127.0.0.1:8000/api/fixed");
-      const data = await response.json();
-      setgetfixed(data.message);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
 
-  const fetchDataRecurring = async () => {
-    try {
-      const response = await fetch("http://127.0.0.1:8000/api/recurring");
-      const data = await response.json();
-      setGetRecurring(data.message);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+  //to fetch data 
 
-  //to fetch data for recurring
-  const [getRecurring, setGetRecurring] = useState([]);
   useEffect(() => {
-    fetchDataFixed();
-    fetchDataRecurring();
+    getfixed();
+    getRecurring();
   }, []);
-  // delete Fixed Part
 
-  const deleteF = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(
-        `http://127.0.0.1:8000/api/fixed/${editPopupId}`,
-        {
-          method: "DELETE",
-          mode: "cors",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const result = await response.json();
-      setdeleteFixed((prev) => prev + 1);
-      setEditPopupId(null);
-      fetchDataFixed();
-    } catch (error) {
-      console.log(error);
-    }
+  const getfixed = async () => {
+    const res = await axios.get("http://127.0.0.1:8000/api/fixed")
+    setfixedData(res.data.message);
+  }
+
+
+
+  const getRecurring = async () => {
+    const res = await axios.get("http://127.0.0.1:8000/api/recurring")
+    setRecurringData(res.data.message);
+  }
+
+
+  // delete Fixed & Recurring Part
+  const handleDelete = async (id) => {
+    // setShowConfirmation(true);
+    await axios.delete(`http://127.0.0.1:8000/api/fixed/${id}`);
+    await axios.delete(`http://127.0.0.1:8000/api/recurring/${id}`);
+    getfixed();
+    getRecurring();
   };
 
-  // Delete Recurring Part
-  const deleteR = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(
-        `http://127.0.0.1:8000/api/recurring/${editPopupId}`,
-        {
-          method: "DELETE",
-          mode: "cors",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const result = await response.json();
-      setdeleteRecurring((prev) => prev + 1);
-      setEditPopupId(null);
-      fetchDataRecurring();
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
-  const editPopFn = (e) => {
-    setButtonPopup(true);
-    setEditPopupId(e.target.value);
-    console.log("editpopup ", e.target.value);
-  };
+
 
   return (
     // InPut Section
     <div className="in-report-body">
       <div className="in-report-card">
-        <form
-          className="income-form"
-          autoComplete="off"
-          onSubmit={handleSubmit}
-        >
-          <div className="input-form">
-            <p>Type</p>
-            <label htmlFor="title"></label>
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Title"
-            ></input>
-            <label htmlFor="amount"></label>
-            <input
-              type="number"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              placeholder="amount"
-            ></input>
-            <label htmlFor="startDate">Start Date:</label>
-            <input
-              type="date"
-              value={startdate}
-              onChange={(e) => setstartDate(e.target.value)}
-            ></input>
-            <label htmlFor="endDate">End Date:</label>
-            <input
-              type="date"
-              value={enddate}
-              onChange={(e) => setendDate(e.target.value)}
-            ></input>
-            <label for="category"></label>
-            <select
-              name="category"
-              id="category"
-              onChange={(e) => setCategory(e.target.value)}
-            >
-              <option>Select an option</option>
-              <option value="fixed">Fixed</option>
-              <option value="recurring">Recurring</option>
-            </select>
+        <div className="income-ex-input">
+          <form
+            className="income-form"
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="title"> Title:
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Title"
+              ></input></label>
+            <label htmlFor="amount">Amount:
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="amount"
+              ></input></label>
+
+            <label htmlFor="startDate">Start Date:
+              <input
+                type="date"
+                value={startdate}
+                onChange={(e) => setstartDate(e.target.value)}
+              ></input></label>
+            <label htmlFor="endDate">End Date:
+              <input
+                type="date"
+                value={enddate}
+                onChange={(e) => setendDate(e.target.value)}
+              ></input></label>
+            <label htmlFor="category">  Category:
+              <select
+                name="category"
+                id="category"
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option>Select Category</option>
+                <option value="fixed">Fixed</option>
+                <option value="recurring">Recurring</option>
+              </select></label>
             <button className="add-btn" type="submit" value="Submit">
               Add
             </button>
-          </div>
-        </form>
-        {/* Fixed Part */}
-        <div>
-          <tr className="report">
-            {getfixed.map((item, index) => (
-              <div className="td-within" key={index}>
-                <td className="type">{item.type}</td>
-                <td className="title">{item.title}</td>
-                <td className="amount">{item.amount}</td>
-                <td className="startDate">{item.startDate}</td>
-                <td className="endDate">{item.endDate}</td>
-                <td className="category">
-                  {item.category} {item.id}
-                </td>
-                <div>
-                  <td>
-                    <button
-                      className="popup-open"
-                      value={item.id}
-                      onClick={(e) => editPopFn(e)}
-                    >
-                      X
-                    </button>
-                  </td>
-                  <Popup
-                    trigger={buttonPopup}
-                    setTrigger={setButtonPopup}
-                    item={item.id}
-                  >
-                    <div>
-                      <button
-                        value={item.id}
-                        className="delete"
-                        onClick={(e) => deleteF(e)}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </Popup>
-                </div>
-                <td>
-                  <button className="edit" onClick={() => setEditpopup(true)}>
-                    Edit
-                  </button>
-                </td>
-                <Editpopup trigger={editpopup} setTrigger={setEditpopup}>
-                  <div></div>
-                </Editpopup>
-              </div>
-            ))}
-          </tr>
+          </form>
         </div>
-        {/* Recurring Part */}
-        <div>
-          <tr className="report">
-            {getRecurring.map((item, index) => (
-              <div className="td-within" key={index}>
-                <td className="type">{item.type}</td>
-                <td className="title">{item.title}</td>
-                <td className="amount">{item.amount}</td>
-                <td className="startDate">{item.startDate}</td>
-                <td className="endDate">{item.endDate}</td>
-                <td className="category">
-                  {item.category} {item.id}
-                </td>
-                <div>
-                  <td>
-                    <button
-                      className="popup-open"
-                      value={item.id}
-                      onClick={(e) => editPopFn(e)}
-                    >
-                      X
-                    </button>
-                  </td>
-                  <Popup
-                    trigger={buttonPopup}
-                    setTrigger={setButtonPopup}
-                    item={item.id}
-                  >
-                    <div>
-                      <button
-                        value={item.id}
-                        className="delete"
-                        onClick={(e) => deleteR(e)}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </Popup>
-                </div>
-                <td>
-                  <button className="edit" onClick={() => setEditpopup(true)}>
-                    EditsetButtonPopup
-                  </button>
-                </td>
 
-                <Editpopup
-                  id={item["id"]}
-                  trigger={editpopup}
-                  setTrigger={setEditpopup}
-                ></Editpopup>
-              </div>
-            ))}
-          </tr>
+
+
+
+
+
+        {/* data Mapping Section */}
+        <div className="inc-ex-data">
+          {/* Fixed Part */}
+          {fixedData.map((item, index) => (
+            <div key={index}>
+              <h3>{item.type}</h3>
+              <h3>{item.title}</h3>
+              <h3>{item.amount}</h3>
+              <h3>{item.startDate}</h3>
+              <h3>{item.endDate}</h3>
+              <h3>
+                {item.category}
+              </h3>
+              <button type="button" onClick={() => handleDelete(item.id)}>Delete</button>
+            </div>
+
+          ))}
+
+          {/* Recurring Part */}
+          {RecurringData.map((item, index) => (
+            <div key={index}>
+              <h4>{item.type}</h4>
+              <h4>{item.title}</h4>
+              <h4>{item.amount}</h4>
+              <h4>{item.startDate}</h4>
+              <h4>{item.endDate}</h4>
+              <h4>
+                {item.category}
+              </h4>
+              <button type="button" onClick={() => handleDelete(item.id)}>Delete</button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
