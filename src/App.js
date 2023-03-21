7d5d0f7fd941a7145185ebaba9c7dd474c53292a
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-03-21 23:08:06 +0200

    Testttt

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Login />} exact />
         <Route element={useAuth() ? <Outlet /> : <Navigate to="/" />}>
