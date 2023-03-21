5b797804def48c893c143c705d4ac9b5403e7219
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-03-22 00:44:11 +0200

    Testttttt

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/test">
       <App />
     </BrowserRouter>
 
